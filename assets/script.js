c2de49ff6a307dc490ea752c542efe22710b1dfb
Athos13 <athosmachadomuniz@gmail.com>
2023-07-06 16:00:31 -0300

    alterações menu mobile e padding seção inicio

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,10 @@
 const menuBurguer=document.getElementsByTagName("button")[0]
 const ulLinksNav=document.querySelector("nav ul")
+const linksNav=document.querySelectorAll("nav ul a") 
 const barraPrimeiraBotao=document.querySelector("button .barra1")
 const barraCentralBotao=document.querySelector("button .barra2")
 const barraTerceiraBotao=document.querySelector("button .barra3")
 
-
-console.log(barraPrimeiraBotao)
 
 menuBurguer.addEventListener('click', ()=>{
     ulLinksNav.classList.toggle("mostrar")
@@ -13,4 +12,14 @@ menuBurguer.addEventListener('click', ()=>{
     barraPrimeiraBotao.classList.toggle("inclina-primeiraDiv-botao")
     barraTerceiraBotao.classList.toggle("inclina-terceiraDiv-botao")
     
+})
+
+linksNav.forEach((link)=>{
+    link.addEventListener('click',()=>{
+        ulLinksNav.classList.toggle("mostrar")
+
+        barraCentralBotao.classList.toggle("esconde-divCentral-botao")
+        barraPrimeiraBotao.classList.toggle("inclina-primeiraDiv-botao")
+        barraTerceiraBotao.classList.toggle("inclina-terceiraDiv-botao")
+    })
 })
