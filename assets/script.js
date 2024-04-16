5410ef53cfb3a4a114d65354a6063f391ffa3c8f
Athos13 <athosmachadomuniz@gmail.com>
2024-04-16 19:01:01 -0300

    adiciona animação de scroll a sessão sobre

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ const linksNav=document.querySelectorAll("nav ul a")
 const barraPrimeiraBotao=document.querySelector("button .barra1")
 const barraCentralBotao=document.querySelector("button .barra2")
 const barraTerceiraBotao=document.querySelector("button .barra3")
-
+const imgTextSobre = document.getElementsByClassName("sobre-conteudo")[0]
 
 menuBurguer.addEventListener('click', ()=>{
     ulLinksNav.classList.toggle("mostrar")
@@ -23,3 +23,14 @@ linksNav.forEach((link)=>{
         barraTerceiraBotao.classList.toggle("inclina-terceiraDiv-botao")
     })
 })
+/*Checa se sessão Sobre está aparecendo na tela ao dar scroll, se sim adiciona animação*/
+function animaImgTextSobre(){
+    const posicao = imgTextSobre.getBoundingClientRect()//retorna coordenadas do elemento
+    const topo = posicao.top//pega a posição topo do elemento
+    const base = posicao.bottom-200//pega a posição final do elemento e subtrai 200px
+    
+    if((topo >= 0) && (base <= window.innerHeight)){//window.innerHeight retorna altura em px da janela
+        imgTextSobre.classList.add("anima-opacidade")
+    }
+}
+document.addEventListener('scroll',animaImgTextSobre)
